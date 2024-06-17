4eb8d9869a997ca3e8a2005b8c854c0af8730aa2
Juan Issac <25804691+idegollado@users.noreply.github.com>
2024-06-17 10:56:00 -0600

    Updates

---
+++ precache-manifest.e9ff846aa96d1dcf0bfd1d4f37009f7d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed8f0e19b378f8c04af5a86ed7f522b",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "621b2f3e88f67dac6b7c",
+    "url": "/react-gh-pages/static/js/10.32c15db9.chunk.js"
+  },
+  {
+    "revision": "f042e40696eaac5eeab7",
+    "url": "/react-gh-pages/static/js/11.7d751c9c.chunk.js"
+  },
+  {
+    "revision": "e56cdd5a48d628af7197",
+    "url": "/react-gh-pages/static/js/2.5a3c0652.chunk.js"
+  },
+  {
+    "revision": "248e8de01e2bbb4e192fd513a6f8c548",
+    "url": "/react-gh-pages/static/js/2.5a3c0652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f6a96eb5701b34ecbc7",
+    "url": "/react-gh-pages/static/js/3.1c2522be.chunk.js"
+  },
+  {
+    "revision": "5f156829275fd646d999",
+    "url": "/react-gh-pages/static/js/4.318e2b5c.chunk.js"
+  },
+  {
+    "revision": "ea016566f6828370b1dc",
+    "url": "/react-gh-pages/static/js/5.81b5efa9.chunk.js"
+  },
+  {
+    "revision": "0a838f49a2dcffed0de9",
+    "url": "/react-gh-pages/static/js/6.5bd935d8.chunk.js"
+  },
+  {
+    "revision": "ea1521a02f55e338e81a",
+    "url": "/react-gh-pages/static/js/7.9654d864.chunk.js"
+  },
+  {
+    "revision": "bd635aa56b82dece0aac",
+    "url": "/react-gh-pages/static/js/8.7210c79f.chunk.js"
+  },
+  {
+    "revision": "313ce5a62b96f14bddba",
+    "url": "/react-gh-pages/static/js/9.6002a154.chunk.js"
+  },
+  {
+    "revision": "b8bc0f8c5f3f2efec158",
+    "url": "/react-gh-pages/static/js/main.b929d071.chunk.js"
+  },
+  {
+    "revision": "1416e99518981645a5e8",
+    "url": "/react-gh-pages/static/js/runtime-main.579acf99.js"
+  }
+]);
